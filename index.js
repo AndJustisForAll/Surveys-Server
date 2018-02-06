50cf535dffb344ac9c9545a237b011a4d6f58a44
amunoz <amunozdv@gmail.com>
2018-02-05 19:23:11 -0600

    added mongodb + finished up auth with passport.js

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const mongoose = require('mongoose');
+require('./models/User');
 require('./services/passport');
 const keys = require('./config/keys');
 const authRoutes = require('./routes/authRoutes');
 
 mongoose.connect(keys.mongoURI);
 const app = express();
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
--- services/passport.js
+++ services/passport.js
@@ -1,15 +1,36 @@
 const passport = require('passport');
+const mongoose = require('mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id); //id from Mongo record, not googleID
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user); 
+    }); 
+});
+
+const User = mongoose.model('users');
 passport.use(new GoogleStrategy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleSecretID,
         callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-        console.log(accessToken);
-        console.log(refreshToken);
-        console.log(profile);
-        done();
+        User.findOne({
+            googleID: profile.id
+        }).then((existingUser) => {
+            if (existingUser) {
+                done(null, existingUser);
+            } else {
+                new User({
+                    googleID: profile.id,
+                    name: profile.displayName
+                }).save().then((user => done(null, user)));
+            }
+        });
     }
 ));
