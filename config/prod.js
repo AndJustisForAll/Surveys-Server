f53568529678846ab48d82e8fea1001318d92823
amunoz <amunozdv@gmail.com>
2018-02-09 15:36:31 -0600

    setup for prod vs dev keys

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleSecretID: process.env.GOOGLE_SECRET_ID,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+};
