fbe9459ef02c909623aabaaaea333cf8ae513b8e
amunoz <amunozdv@gmail.com>
2018-04-01 22:30:44 -0500

    Angular project setup

--- ng-emaily/src/app/app.module.ts
+++ ng-emaily/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { TestComponent } from './test/test.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+  AppComponent,
+  TestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
